84176eac127bd3ff762143305181e378f40871a8
adianbu <57458067+adianbu@users.noreply.github.com>
2020-06-22 17:33:02 +0530

    Final commit, code could be shrunk

--- index.js
+++ index.js
@@ -4,39 +4,42 @@ var dec = document.querySelector('.decrease');
 var res = document.querySelector('.reset');
 var s = -1;
 
-function increase(){
-s=0;
-    console.log("clicked increase");
-    setInterval(()=>
-    {
-      if(s== 0)
-      {
-        //if only code below is executed : one click will add 1 to the current value 
-        var value = parseInt(document.getElementById('number').value, 10);
-        value = isNaN(value) ? 0 : value;
-        value++;
-        document.getElementById('number').value = value;
-        document.getElementById('number').textContent = value;
-      }
+var increasefunction = setInterval(()=>
+{
+  if(s== 0)
+  {
+    //if only code below is executed : one click will add 1 to the current value 
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+    document.getElementById('number').textContent = value;
+  }
+},1000);
 
-    },1000);
-    
+var decreasefunction = setInterval(()=>
+{
+  if(s == 1)
+  {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('number').value = value;
+    document.getElementById('number').textContent = value;
+  }
+},1000);
+
+function increase(){
+    s=0;
+    console.log("clicked increase");
+    increasefunction(); 
+    clearInterval(increasefunction);
 }
 
 function decrease(){
     s=1;
-    setInterval(()=>
-    {
-      if(s == 1)
-      {
-        var value = parseInt(document.getElementById('number').value, 10);
-        value = isNaN(value) ? 0 : value;
-        value--;
-        document.getElementById('number').value = value;
-        document.getElementById('number').textContent = value;
-      }
-
-    },1000);
+    decreasefunction();
+    clearInterval(decreasefunction);
 
 }
 
